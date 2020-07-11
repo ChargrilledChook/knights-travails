4c26cdba8a1909ee86c23f3f5b5e1c8bd5bb91fa
Blackett <happyfrog578+github@gmail.com>
2020-07-11 17:46:43 +1000

    add board prototype

---
+++ board.rb
@@ -0,0 +1,19 @@
+def board
+	row = Array (0..7)
+	col = []
+	8.times { col << (Array (0..7) )}
+	col
+end
+
+def knight(x, y)
+	position = [x, y]
+end
+pp board
+
+def place_knight(x, y, board)
+	board[x][y] = 'K'
+	board
+end
+
+new_board = board
+pp place_knight(4,4,new_board)
